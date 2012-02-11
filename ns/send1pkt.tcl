d8993c702073b8acaaf787b03b3a284fc8d4e9a5
z-force <andrew.gucalyuk@gmail.com>
2012-02-11 19:10:23 +0400

    send1pkt.tcl: add router

--- ns/send1pkt.tcl
+++ ns/send1pkt.tcl
@@ -3,14 +3,21 @@ set ns [new Simulator]
 $ns namtrace-all [open out.nam w]
 # Создать узлы сети (компьютеры)
 set sender [$ns node] ; # отправитель
+set router [$ns node] ; # роутер
 set receiver [$ns node] ; # получатель
 # Линия связи между узлами
-$ns duplex-link $sender $receiver 10Mb 2ms DropTail
+$ns duplex-link $sender $router 10Mb 2ms DropTail
+$ns duplex-link $router $receiver 10Mb 2ms DropTail 
+
 # Настроить соединение по протоколу UDP
 set udpAgent [$ns create-connection UDP $sender Null $receiver 1]
 # По истечении 1 мсек симуляции отправить пакет в 1000 байт
 $ns at 0.001 "$udpAgent send 1000"
 # По истечении 1 сек запустить проигрыватель визуализации
 $ns at 1.0 "exec nam -r 100us out.nam &"
+$ns gen-map
+#$ns halt
+#$ns dumpq
+
 # Стартовать симуляцию
 $ns run 
